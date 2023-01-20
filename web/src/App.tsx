8060c604a100467b65f40f0f4fa77108f3d40a81
Juliana Morikoshi <julianamorikoshi@gmail.com>
2023-01-20 00:35:48 -0300

    feat(web): add home header and table components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,16 @@
 import './styles/global.css'
 
-import { Habit } from './components/Habit'
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 
-function App(): JSX.Element {
+export function App(): JSX.Element {
   return (
-    <>
-      <Habit completed={2} />
-      <Habit completed={4} />
-      <Habit completed={6} />
-      <Habit completed={8} />
-      <Habit completed={10} />
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+
+        <SummaryTable />
+      </div>
+    </div>
   )
 }
-
-export default App
